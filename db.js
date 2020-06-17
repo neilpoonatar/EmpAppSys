551316c167341bbbbabe5471d3315b48c52af23f
neilpoonatar <neil.poonatar@gmail.com>
2020-06-17 11:08:36 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/crudDB", (err) => {
+  if (!err) console.log("MongoDB connection succeeded.");
+  else
+    console.log(
+      "Error in DB connection : " + JSON.stringify(err, undefined, 2)
+    );
+});
+
+module.exports = mongoose;
